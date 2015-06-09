1fd03a4bbdc2b4d40ddb14115fdcef55c67ab898
Eddie <adward_r@icloud.com>
2015-06-09 17:14:59 +0800

    Revert 089fc44..c9e8780
    This rolls back to commit 089fc44517e8ada159b96024c8b45ba907684376.

--- djcode/djcode/settings.py
+++ djcode/djcode/settings.py
@@ -38,12 +38,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'IMS',
+    'RMS',
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
